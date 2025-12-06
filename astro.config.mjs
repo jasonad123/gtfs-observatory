8f638c7c9d38a5aed6a09c366f91770aab24763d
jasonad123 <me@jasonadle.com>
2025-12-06 01:38:37 -0500

    Install wrangler and Astro adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   site: 'https://dmv-gtfs-dev.pages.dev',
   output: 'static',
+
   build: {
     assets: 'assets'
-  }
+  },
+
+  adapter: cloudflare()
 });
